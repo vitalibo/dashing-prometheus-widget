be4fd2ec2aeb2f8fde80f8e7a0503c64970be207
Vitalii Boiarskyi2 <vitalii_boiarskyi2@epam.com>
2017-04-13 17:22:49 +0300

    Configure job scrape Prometheus metrics

--- src/jobs/prometheus.rb
+++ src/jobs/prometheus.rb
@@ -1,14 +1,19 @@
-# Populate the graph with some random points
-points = []
-(1..10).each do |i|
-  points << { x: i, y: rand(50) }
-end
-last_x = points.last[:x]
+require 'net/https'
+require 'json'
 
-SCHEDULER.every '2s' do
-  points.shift
-  last_x += 1
-  points << { x: last_x, y: rand(50) }
+SCHEDULER.every '15s', :first_in => 0 do |job|
+  begin
+    http = Net::HTTP.new('prometheus', 9090)
+    request = Net::HTTP::Get.new('/api/v1/query_range?query=scrape_duration_seconds&start=2017-04-13T00:00:00.000Z&end=2017-04-20T00:00:00.000Z&step=20s')
+    response = http.request(request)
 
-  send_event('prometheus', points: points)
+    points = []
+    JSON.parse(response.body)['data']['result'][0]['values'].each do |node|
+      points.push( {:x => node[0], :y => node[1].to_f} )
+    end
+
+    send_event('scrape_duration_seconds', points: points)
+  rescue => e
+    puts "Error: #{e}"
+  end
 end
